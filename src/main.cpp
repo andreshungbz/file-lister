8427083dbc3d64ab7339c1e859949f6481351aef
Andres Hung <andres.hung@outlook.com>
2024-08-06 14:07:34 -0600

    Add support for command-line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include "file-utils.h"
+#include <iostream>
 
-int main() {
-    listFiles(getDirectory());
+int main(int argc, char* argv[]) {
+    std::filesystem::path directory;
+    bool reprompt{false};
+
+    if (argc > 1) {
+        directory = std::filesystem::path(argv[1]);
+
+        if (!std::filesystem::exists(directory) || !std::filesystem::is_directory(directory)) {
+            std::cout << "Invalid directory. Try again.\n";
+            reprompt = true;
+        }
+    }
+
+    if (argc == 1 || reprompt) {
+        directory = getDirectory();
+    }
+
+    listFiles(directory);
     return 0;
 }
